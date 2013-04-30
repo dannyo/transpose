48f5b9e8dfebbb6d0445a9c2e1e93841dc1501d7
danny <dannyorh@gmail.com>
2013-04-30 14:22:23 -0700

    transpose - (Danny) - updated route for now to avoid / discrepancies

--- transpose/src/com/project/transpose/IndexServlet.java
+++ transpose/src/com/project/transpose/IndexServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/")
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
 	private static final String VIEWS_INDEX_JSP = "/views/index.jsp";
 	private static final long serialVersionUID = 1L;
